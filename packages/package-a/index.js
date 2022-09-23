69349aff560848d05efed1f9cb7af27df0184e89
zqinmiao <zqinmiao@gmail.com>
2022-09-23 15:53:28 +0800

    feat: 做一些修改

--- packages/package-a/index.js
+++ packages/package-a/index.js
@@ -1,3 +1,4 @@
 const { isArray } = require("lodash");
 
 console.log("isArray", isArray(""), isArray([]));
+console.log("2022-09-23");
--- packages/pnpm-lerna-1/index.js
+++ packages/pnpm-lerna-1/index.js
@@ -1,1 +1,2 @@
 console.log("log: @buibis/pnpm-lerna-1");
+console.log("2022-09-23");
